7ee29ebf86b2b5a9c48416c378095bc7d804907a
Mabel <mariaizabeldelima0@gmail.com>
2026-09-11 22:24:49 -0300

    define esquema de validacao do pagamento com Zod

---
+++ src/utils/validacaoPagamento.js
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+
+export const esquemaPagamento = z.object({
+  titular: z
+    .string()
+    .trim()
+    .min(3, 'Informe o nome do titular como está no cartão.'),
+
+  cartao: z
+    .string()
+    .transform((valor) => valor.replace(/[\s-]/g, ''))
+    .refine((valor) => /^\d{16}$/.test(valor), 'O cartão precisa ter 16 dígitos.'),
+
+  validade: z
+    .string()
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, 'Use o formato MM/AA.'),
+
+  cvv: z.string().regex(/^\d{3}$/, 'O CVV precisa ter 3 dígitos.'),
+})
